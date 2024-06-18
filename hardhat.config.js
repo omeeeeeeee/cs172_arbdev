b35ffb66b43fbefff3446d43770af2eeb955ae9e
omeeeeeeee <naamparo@up.edu.ph>
2024-06-18 17:57:30 +0800

    added svelte ui app and deployed contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
+
+const { PRIVATE_KEY, JSON_RPC_URL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: `${JSON_RPC_URL}`,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
 };
